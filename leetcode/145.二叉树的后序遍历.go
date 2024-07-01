1eb5eeb72413be8b300930f39a6a0c16d74f4c17
Kevin Shan <779973772@qq.com>
2024-07-01 20:35:02 +0800

    update code

--- leetcode/145.二叉树的后序遍历.go
+++ leetcode/145.二叉树的后序遍历.go
@@ -1,9 +1,9 @@
 package main
 
 /*
- * @lc app=leetcode.cn id=144 lang=golang
+ * @lc app=leetcode.cn id=145 lang=golang
  *
- * [144] 二叉树的前序遍历
+ * [145] 二叉树的后序遍历
  */
 
 // @lc code=start
@@ -15,39 +15,23 @@ package main
  *     Right *TreeNode
  * }
  */
-func preorderTraversal(root *TreeNode) []int {
+func postorderTraversal(root *TreeNode) []int {
 	res := make([]int, 0)
 	var preOrder func(root *TreeNode)
 	preOrder = func(root *TreeNode) {
 		if root == nil {
 			return
 		}
-		res = append(res, root.Val)
 		preOrder(root.Left)
 		preOrder(root.Right)
+		res = append(res, root.Val)
 
 	}
 	preOrder(root)
 	return res
 }
 
-func preorderTraversal1(root *TreeNode) []int {
-	stack := make([]*TreeNode, 0)
-	res := make([]int, 0)
-	node := root
-	for node != nil || len(stack) > 0 {
-		for node != nil {
-			stack = append(stack, node)
-			res = append(res, node.Val)
-			node = node.Left
-		}
-		node = stack[len(stack)-1].Right
-		stack = stack[:len(stack)-1]
-	}
-	return res
-}
-
-func preorderTraversal2(root *TreeNode) []int {
+func postorderTraversal1(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
@@ -58,14 +42,31 @@ func preorderTraversal2(root *TreeNode) []int {
 		node := stack[len(stack)-1]
 		stack = stack[:len(stack)-1]
 		res = append(res, node.Val)
-		if node.Right != nil {
-			stack = append(stack, node.Right)
-		}
+
 		if node.Left != nil {
 			stack = append(stack, node.Left)
 		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
 	}
+
+	reverseList(res)
 	return res
+}
+
+func reverseList(arr []int) {
+	if len(arr) < 2 {
+		return
+	}
+	i, j := 0, len(arr)-1
+	for i < j {
+		temp := arr[i]
+		arr[i] = arr[j]
+		arr[j] = temp
+		i++
+		j--
+	}
 }
 
 // @lc code=end
